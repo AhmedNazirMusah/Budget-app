fe9de966ad5e946bea9d078f091970eba3fa48ff
ahmednazirmusah <ahmed999nazir@gmail.com>
2023-02-21 12:57:40 +0000

    Update group controller and index page

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,12 @@ class Group < ApplicationRecord
     belongs_to :user
     has_many :entities
     validates :name, :user, presence: true
+
+    def amount_accumulated_amount
+        total = 0
+        entities.each do |e|
+          total += e.amount
+        end
+        total
+      end
 end
